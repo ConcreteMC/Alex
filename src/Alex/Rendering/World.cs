78bfd42c764e0c09d39c0b39ef85ba04d1820ded
Kenny van Vulpen <kennyvvulpen@gmail.com>
2016-04-26 01:07:43 +0200

    Remove terrain generators

--- src/Alex/Rendering/World.cs
+++ src/Alex/Rendering/World.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Alex.Graphics.Items;
-using Alex.Terrain;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -13,13 +12,11 @@ namespace Alex.Rendering
         public World()
         {
             ChunkManager = new ObjectManager();
-            TerrainGenerator = new FlatTerrainGenerator("Flatlands");
 		    //TerrainGenerator = new DefaultTerrainGenerator("lol");
             Logging.Info("Generating chunks...");
 			//GenerateChunks(GetSpawnPoint(), (int)Game.RenderDistance);
         }
 
-		private TerrainGenerator TerrainGenerator { get; set; }
         public ObjectManager ChunkManager { get; private set; }
 
         internal int Vertices
@@ -47,68 +44,9 @@ namespace Alex.Rendering
             ChunkManager.Draw(Game.GraphicsDevice);
         }
 
-        private readonly object _chunkLock = new object();
-        public bool GenerateChunks(Vector3 chunkPosition, int viewDistance)
-        {
-            lock (_chunkLock)
-            {
-                var newOrders = new Dictionary<Vector3, double>();
-                var radiusSquared = viewDistance / Math.PI;
-                var radius = Math.Ceiling(Math.Sqrt(radiusSquared));
-                var centerX = (int)chunkPosition.X;
-                var centerZ = (int)chunkPosition.Z;
-
-                for (var x = -radius; x <= radius; ++x)
-                {
-                    for (var z = -radius; z <= radius; ++z)
-                    {
-                        var distance = (x*x) + (z*z);
-                        if (distance > radiusSquared)
-                        {
-                            continue;
-                        }
-                        var chunkX = (int) (x + centerX);
-                        var chunkZ = (int) (z + centerZ);
-
-                        var index = new Vector3(chunkX, 0, chunkZ);
-                        newOrders[index] = distance;
-                    }
-                }
-
-                if (newOrders.Count > viewDistance)
-                {
-                    foreach (var pair in newOrders.OrderByDescending(pair => pair.Value))
-                    {
-                        if (newOrders.Count <= viewDistance) break;
-                        newOrders.Remove(pair.Key);
-                    }
-                }
-
-                var renderChunks = ChunkManager.Chunks;
-                foreach (var chunkKey in renderChunks.Keys)
-                {
-                    if (!newOrders.ContainsKey(chunkKey))
-                    {
-                        ChunkManager.RemoveChunk(chunkKey);
-                    }
-                }
-
-                foreach (var pair in newOrders.OrderBy(pair => pair.Value))
-                {
-                    if (renderChunks.ContainsKey(pair.Key)) continue;
-
-                    var chunk = TerrainGenerator.GenerateChunk(pair.Key);
-
-                    ChunkManager.AddChunk(chunk, pair.Key);
-                }
-            }
-
-            return true;
-        }
-
         public Vector3 GetSpawnPoint()
         {
-            return TerrainGenerator.GetSpawnPoint();
+            return Vector3.Zero;
         }
 
 	    public bool IsSolid(Vector3 location)
