d7b7219a63c91837c5d37550ccd21dcf0bbb4011
Dan Spiteri <trudan97@gmail.com>
2018-04-28 07:02:24 +0200

    .net core launcher

--- src/Alex.Launcher/Program.cs
+++ src/Alex.Launcher/Program.cs
@@ -1,26 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
+using Alex.Launcher.ViewModels;
+using Alex.Launcher.Views;
+using Avalonia;
+using Avalonia.Logging.Serilog;
 
 namespace Alex.Launcher
 {
-    public static class Program
+    class Program
     {
 
-        [STAThread]
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             if (!args.Any())
             {
-                // Run Launcher
-                App.Main();
+                BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
             }
             else
             {
@@ -28,54 +25,57 @@ namespace Alex.Launcher
                 RunAlex(new string[] { });
             }
         }
+
+        public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
+                                                        .UsePlatformDetect()
+                                                        .UseReactiveUI()
+                                                        .LogToDebug();
         public static void RunAlex() => RunAlex(new string[0]);
 
         public static void RunAlex(string[] args)
         {
             try
             {
-                var currentDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                var currentDir  = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
                 var alexDllPath = Path.Combine(currentDir, "Alex.dll");
-                var alexArgs = string.Join(" ", args);
+                var alexArgs    = string.Join(" ", args);
                 
                 var cmd = $"dotnet \"{alexDllPath}\" {alexArgs}";
                 var processStartInfo = new ProcessStartInfo()
                 {
-                    FileName = "dotnet.exe",
-                    Arguments = $"\"{alexDllPath}\" {alexArgs}",
-                    UseShellExecute = false,
+                    FileName         = "dotnet.exe",
+                    Arguments        = $"\"{alexDllPath}\" {alexArgs}",
+                    UseShellExecute  = false,
                     WorkingDirectory = currentDir,
-                    CreateNoWindow = true,
+                    CreateNoWindow   = true,
                     
                     //WindowStyle = ProcessWindowStyle.Hidden
                 };
 
                 using(var alex = Process.Start(processStartInfo)) {
-                if (alex != null && !alex.HasExited)
-                {
-                    if (Application.Current.MainWindow != null) Application.Current.MainWindow.Hide();
-                }
-                
-                //MessageBox.Show($"Opened with pid {alex.Id}!");
-
-                //Thread.Sleep(5 * 1000);
-
-                //MessageBox.Show("Closed!");
-                
-                alex.WaitForExit();
-                var main = Application.Current.MainWindow;
-                if (main != null)
-                {
-                    main.Show();
-                    main.Focus();
-                }
+                    if (alex != null && !alex.HasExited)
+                    {
+                        MainWindow.Instance?.Hide();
                     }
+                
+                    //MessageBox.Show($"Opened with pid {alex.Id}!");
+
+                    //Thread.Sleep(5 * 1000);
+
+                    //MessageBox.Show("Closed!");
+                
+                    alex.WaitForExit();
+                    if (MainWindow.Instance != null)
+                    {
+                        MainWindow.Instance.Show();
+                        MainWindow.Instance.Focus();
+                    }
+                }
             }
             catch (Exception ex)
             {
                 throw;
             }
         }
-
     }
 }
