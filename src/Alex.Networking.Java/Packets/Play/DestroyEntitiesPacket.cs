459617e652fa9bb81f790ab95b4060503303defd
Kenny van Vulpen <kennyvvulpen@ziggo.nl>
2022-01-01 16:49:11 +0100

    Fix DestroyEntitiesPacket for MC:Java

--- src/Alex.Networking.Java/Packets/Play/DestroyEntitiesPacket.cs
+++ src/Alex.Networking.Java/Packets/Play/DestroyEntitiesPacket.cs
@@ -1,4 +1,5 @@
-﻿using Alex.Networking.Java.Util;
+﻿using System.Threading.Tasks;
+using Alex.Networking.Java.Util;
 
 namespace Alex.Networking.Java.Packets.Play
 {
@@ -11,11 +12,16 @@ namespace Alex.Networking.Java.Packets.Play
 
 		public int[] EntityIds;
 
-		public override void Decode(MinecraftStream stream)
+		/// <inheritdoc />
+		public override async Task DecodeAsync(MinecraftStream stream)
 		{
-			EntityIds = new int[1];
-			int count = stream.ReadVarInt();
-			EntityIds[0] = count;
+			int count = await stream.ReadVarIntAsync();
+			EntityIds = new int[count];
+
+			for (int i = 0; i < EntityIds.Length; i++)
+			{
+				EntityIds[i] = await stream.ReadVarIntAsync();
+			}
 		}
 
 		public override void Encode(MinecraftStream stream)
